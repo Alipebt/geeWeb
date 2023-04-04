30d5b4e09f3e2eb087e86bb75f2c755ff37b3a0a
Alipebt <1160756683@qq.com>
2023-04-04 19:18:25 +0800

    add router_test
    Signed-off-by: Alipebt <1160756683@qq.com>

--- main.go
+++ main.go
@@ -13,14 +13,17 @@ func main() {
 	})
 
 	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=geektutu
 		c.String(http.StatusOK, "Hello %s, you`re at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.POST("/login", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{
-			"username": c.PostFrom("username"),
-			"password": c.PostFrom("password"),
-		})
+	r.GET("/hello/:name", func(c *gee.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "Hello %s, you`re at %s\n", c.Params["name"], c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *gee.Context)  {
+		c.JSON(http.StatusOK, gee.H{"filepath": c.Params["filepath"]})
 	})
 
 	r.Run(":9999")
