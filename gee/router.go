2a268eb250e59c3d2c4a483babbc3120e29b7306
Alipebt <1160756683@qq.com>
2023-04-04 11:42:27 +0800

    getRoute
    Signed-off-by: Alipebt <1160756683@qq.com>

--- gee/router.go
+++ gee/router.go
@@ -71,9 +71,11 @@ func (r *router) getRoute(method string, path string) (*node, map[string]string)
 }
 
 func (r *router) handle(c *Context) {
-	key := c.Method + "-" + c.Path
-	if handler, ok := r.handlers[key]; ok {
-		handler(c)
+	n, params := r.getRoute(c.Method, c.Path)
+	if n != nil {
+		c.Params = params
+		key := c.Method + "-" + c.Path
+		r.handlers[key](c)
 	} else {
 		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
 	}
